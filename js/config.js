cfb45a38a75bf1d512c2d3ea7fe030278183fe4d
ChidiEbube123 <fleektyre@gmail.com>
2025-03-05 13:41:39 +0100

    trying spa

--- js/config.js
+++ js/config.js
@@ -1,10 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_KcwScgQ9A', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '13pmvlt5juji179thoo9kbod2b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5c6p9toeepsnqlrqjq8rjb6r8h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
+
